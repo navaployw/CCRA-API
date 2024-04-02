15a40b6d07372415b2fb4e5a572c6d4fc2bcaa1b
dotnanoniwat <dotnano.niwat@gmail.com>
2024-04-02 20:07:02 +0700

    s

--- src/main/java/com/arg/ccra3/online/service/ReportService.java
+++ src/main/java/com/arg/ccra3/online/service/ReportService.java
@@ -239,7 +239,7 @@ private void zipDirectory(File directory, ZipOutputStream zipStream, String pare
         {
             throw new Exception("path file not support.");
         }
-        FileInputStream fis = new FileInputStream("Test");
+        FileInputStream fis = null;//new FileInputStream("Test");
  
         BufferedInputStream bis = new BufferedInputStream(fis);
         String entryName = parent + file.getName();
