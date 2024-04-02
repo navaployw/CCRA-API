1083a32d72e70ed3853e1a935b01c480926c94de
dotnanoniwat <dotnano.niwat@gmail.com>
2024-04-02 17:57:38 +0700

    s

--- src/main/java/com/arg/ccra3/online/service/ReportService.java
+++ src/main/java/com/arg/ccra3/online/service/ReportService.java
@@ -149,9 +149,9 @@ public class ReportService {
             logger.info(">>>>creatReportHTML 2<<<<");
             File htmlFolder = htmlDao.getHtmlFolder(dirPath, expenseId, user);
             logger.info(">>>>creatReportHTML 3<<<<");            
-            // zipToResponse(response, htmlFolder);
+            zipToResponse(response, htmlFolder);
             
-            // FileManagerUtil.deleteDirectory(htmlFolder);
+            FileManagerUtil.deleteDirectory(htmlFolder);
         }
         catch(Exception e){
             logger.error(e.getMessage(),e);
